ad7535bfd76fca29be426b92ca0055aec2930d4b
owndigitaldev <owndigital.dev>
2021-04-05 14:51:22 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This project using [Flutter](https://flutter.dev/)
 with [Kotlin](https://kotlinlang.org/) as native language for [Android](https://www.android.com/)
 .
 
+Rest API by [Fake Store API](https://fakestoreapi.com/).
+
 ##Created by :
 [Rahmat Trinanda](https://www.linkedin.com/in/rahmat-trinanda/)
 
--- lib/page/cart_page.dart
+++ lib/page/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:add_to_cart/model/app/singleton_model.dart';
 import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
@@ -6,8 +7,22 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  GlobalKey<ScaffoldState> _scaffoldKey;
+  SingletonModel _model;
+
+  @override
+  void initState() {
+    super.initState();
+    _scaffoldKey = new GlobalKey<ScaffoldState>();
+    _model = SingletonModel.shared;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Container(),
+    );
+    //untuk mendapatkan data cart, bisa panggil dengan _model.carts
   }
 }
